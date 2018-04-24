348f95d671659908b76b207b13d8bc6ca30e3c19
mike.woods <mike.woods@ncontracts.com>
2018-04-24 13:44:00 -0500

    Fixed Collections request object to use custom object instead of base request

---
+++ src/Client/Bis/CollectionsRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Experian.Api.Client.Bis
+{
+    public class CollectionsRequest : BisRequest
+    {
+        public bool CollectionsSummary { get; set; }
+
+        public bool CollectionsDetail { get; set; }
+    }
+}
