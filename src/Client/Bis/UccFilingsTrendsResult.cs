66e600acd5b469695dc58f5859129d16886b00dc
mike.woods <mike.woods@ncontracts.com>
2018-04-30 14:13:43 -0500

    changed properties from private to public

--- src/Client/Bis/UccFilingsTrendsResult.cs
+++ src/Client/Bis/UccFilingsTrendsResult.cs
@@ -2,16 +2,16 @@ namespace Experian.Api.Client.Bis
 {
     public class UccFilingsTrendsResult
     {
-        private string Date { get; set; }
+        public string Date { get; set; }
 
-        private int Count { get; set; }
+        public int Count { get; set; }
 
-        private int DerogatoryCount { get; set; }
+        public int DerogatoryCount { get; set; }
 
-        private int ReleasesAndTerminationsCount { get; set; }
+        public int ReleasesAndTerminationsCount { get; set; }
 
-        private int ContinuationsCount { get; set; }
+        public int ContinuationsCount { get; set; }
 
-        private int AmendedAndAssignedCount { get; set; }
+        public int AmendedAndAssignedCount { get; set; }
     }
 }
