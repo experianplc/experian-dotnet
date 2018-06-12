d64364cdc7677214cafac41558e8d4ec4b181dfa
Lexcess <Lexcess@users.noreply.github.com>
2018-06-12 16:40:50 +0100

    Made Code Nullable to better reflect responses from the API. (#12)

--- src/Client/Bis/RiskClassResult.cs
+++ src/Client/Bis/RiskClassResult.cs
@@ -2,7 +2,7 @@ namespace Experian.Api.Client.Bis
 {
     public sealed class RiskClassResult
     {
-        public int Code { get; set; }
+        public int? Code { get; set; }
 
         public string Definition { get; set; }
     }
